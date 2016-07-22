926982523abd2a6f13b2e644608a6eaf58c13cd6
Deepika Vasudevan <deepikav2793@gmail.com>
2016-07-22 16:41:06 +0530

    Refactored | Deepika

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,15 +1,16 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-collatzChainNumber := 0	
+	collatzChainNumber := 0	
 	for n != 1 {
 		if n % 2 == 0 {
 			n = n / 2
 		} else {
 			n = 3 * n + 1
 		}
+	
 	collatzChainNumber++
 	}
 
-return collatzChainNumber  
+	return collatzChainNumber  
 }
