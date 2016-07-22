dbc4a7e2c6c52d40e3da15a8dc7f65d41669c89e
Deepika Vasudevan <deepikav2793@gmail.com>
2016-07-22 16:06:46 +0530

    Implemented Collatz Chain logic | Deepika

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,15 @@
 package control_structures
 
 func collatzChainLength(n int) int {
-	return 0
+collatzChainNumber := 0	
+	for n != 1 {
+		if n % 2 == 0 {
+			n = n / 2
+		} else {
+			n = 3 * n + 1
+		}
+	collatzChainNumber++
+	}
+
+return collatzChainNumber  
 }
